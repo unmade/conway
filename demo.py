97f286b9d2e146b3761c7c38f9793af83bf5893b
Aleksei Maslakov <lesha.maslakov@gmail.com>
2018-08-09 00:31:56 +0300

    more patterns

--- demo.py
+++ demo.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
+
 import argparse
 import curses
 
 from drawille import Canvas, animate
 
-from conway import patterns
+from conway.patterns import guns, infinite, methuselah, oscillators
 from conway.life import Grid
 
 PATTERN_MAP = {
-    'one': patterns.ONE,
-    'two': patterns.TWO,
-    'three': patterns.THREE,
-    'glider': patterns.GLIDER,
-    'gosper': patterns.GOSPER,
+    'one': infinite.ONE,
+    'two': infinite.TWO,
+    'three': infinite.THREE,
+    'gosper': guns.GOSPER,
+    'simkin': guns.SIMKIN,
+    'pentomino': methuselah.THE_R_PENTOMINO,
+    'diehard': methuselah.DIEHARD,
+    'acorn': methuselah.ACORN,
+    'pulsar': oscillators.PULSAR,
+    'pentadecathlon': oscillators.PENTADECATHLON,
 }
 
 
@@ -22,6 +28,12 @@ def build_parser():
     _parser.add_argument(
         'pattern',
         choices=PATTERN_MAP.keys(),
+    )
+
+    _parser.add_argument(
+        '--speed',
+        type=float,
+        default=0.1,
     )
 
     return _parser
@@ -35,9 +47,12 @@ def play(scr, pattern):
         m=y * 2 - 10
     )
 
+    i = 0
     while True:
+        scr.addstr(x - 1, 0, f'generation: {i}')
         grid.tick()
         yield [(0, 0), ] + [(y, x) for x, y in grid.live_cells]
+        i += 1
 
 
 if __name__ == '__main__':
@@ -49,4 +64,4 @@ if __name__ == '__main__':
 
     canvas = Canvas()
 
-    animate(canvas, play, .1, stdscr, PATTERN_MAP[args.pattern])
+    animate(canvas, play, args.speed, stdscr, PATTERN_MAP[args.pattern])
